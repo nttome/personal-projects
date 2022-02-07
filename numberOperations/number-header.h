b8aadaf3f0ccb9ce3046a5bf4f9ad1f94e590c2d
Nicole Tome <97120660+nttome@users.noreply.github.com>
2022-02-07 15:29:47 -0500

    Add files via upload

---
+++ numberOperations/number-header.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+// Functional Prototypes
+
+int reverse_number(int num);
+// Reverse an n-digit number, where n can be any positive integer 
+
+int reverse_number_do_while(int num);
+// use the do-while loop
+
+int even_odd(int num);
+// Determine if the number is even or odd
+
+int is_prime(int num);
+// Determine if the number is prime
+
+
+#endif // NUMBER-SUPPORT_INCLUDED
